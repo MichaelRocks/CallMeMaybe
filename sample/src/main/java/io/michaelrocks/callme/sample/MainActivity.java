0f472d5e07c44822200ccf5edcede849f89983f5
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-03 19:12:40 +0300

    Add a sample project for the library

---
+++ sample/src/main/java/io/michaelrocks/callme/sample/MainActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.michaelrocks.callme.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import io.michaelrocks.callme.CallMe;
+import io.michaelrocks.callme.sample.R;
+
+public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_activity);
+
+    final TextView phoneEditText = (TextView) findViewById(R.id.phoneEditText);
+    CallMe.applyTo(phoneEditText);
+  }
+}
