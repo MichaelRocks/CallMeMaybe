7e05c165e5a8d9d7c00587e1a694c2cb1c42b366
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-03 19:22:04 +0300

    Update copyrights

--- library/src/main/java/io/michaelrocks/callme/PhoneEditText.java
+++ library/src/main/java/io/michaelrocks/callme/PhoneEditText.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2016 Michael Rozumyanskiy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.michaelrocks.callme;
 
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.text.Editable;
 import android.util.AttributeSet;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 
 public class PhoneEditText extends EditText {
