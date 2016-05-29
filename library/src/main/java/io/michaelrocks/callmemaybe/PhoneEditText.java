caba201510358adead94f9cda41d070446f86de6
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2016-05-29 23:26:21 +0300

    Read a region from PhoneEditText attributes

--- library/src/main/java/io/michaelrocks/callmemaybe/PhoneEditText.java
+++ library/src/main/java/io/michaelrocks/callmemaybe/PhoneEditText.java
@@ -18,6 +18,7 @@ package io.michaelrocks.callmemaybe;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.widget.EditText;
@@ -25,22 +26,36 @@ import android.widget.EditText;
 public class PhoneEditText extends EditText {
   public PhoneEditText(final Context context) {
     super(context);
+    initialize(null, 0, 0);
   }
 
   public PhoneEditText(final Context context, final AttributeSet attrs) {
     super(context, attrs);
+    initialize(attrs, 0, 0);
   }
 
   public PhoneEditText(final Context context, final AttributeSet attrs, final int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    initialize(attrs, defStyleAttr, 0);
   }
 
   @TargetApi(Build.VERSION_CODES.LOLLIPOP)
   public PhoneEditText(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
+    initialize(attrs, defStyleAttr, defStyleRes);
   }
 
-  {
-    CallMeMaybe.attachTo(this);
+  private void initialize(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+    final TypedArray array =
+        getContext().obtainStyledAttributes(attrs, R.styleable.PhoneEditText, defStyleAttr, defStyleRes);
+    initializeFromTypedArray(array);
+    array.recycle();
+  }
+
+  private void initializeFromTypedArray(final TypedArray array) {
+    final FormatParameters parameters = FormatParameters.newBuilder()
+        .region(array.getString(R.styleable.PhoneEditText_cmm_region))
+        .build();
+    CallMeMaybe.attachTo(this, parameters);
   }
 }
